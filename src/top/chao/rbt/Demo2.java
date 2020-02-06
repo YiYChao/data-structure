3f169543e5df757c1521d00a27a8f4ecf78338c9
YiYChao <yiychao@foxmail.com>
2020-02-06 19:42:52 +0800

    实现红黑树的基本数据结构设计及完成数据节点的添加； 将二分搜索树、AVL树和红黑树进行性能的对比

---
+++ src/top/chao/rbt/Demo2.java
@@ -0,0 +1,62 @@
+package top.chao.rbt;
+
+import top.chao.avl.AVLTree;
+import top.chao.map.BSTMap;
+
+import java.util.ArrayList;
+import java.util.Random;
+/**
+ *  @Description:随机添加和查询2KW个数据，二分搜索树、AVL树和红黑树的性能对比
+ *  @author: YiYChao
+ *  @Date: 2020/2/6 19:39
+ *  @Version: V1.0
+ */
+public class Demo2 {
+    public static void main(String[] args) {
+
+        int n = 20000000;
+
+        Random random = new Random(n);
+        ArrayList<Integer> testData = new ArrayList<>(n);
+        for(int i = 0 ; i < n ; i ++)
+            testData.add(random.nextInt(Integer.MAX_VALUE));
+
+        // Test BST
+        long startTime = System.nanoTime();
+
+        BSTMap<Integer, Integer> bst = new BSTMap<>();
+        for (Integer x: testData)
+            bst.add(x, null);
+
+        long endTime = System.nanoTime();
+
+        double time = (endTime - startTime) / 1000000000.0;
+        System.out.println("BST: " + time + " s");
+
+
+        // Test AVL
+        startTime = System.nanoTime();
+
+        AVLTree<Integer, Integer> avl = new AVLTree<>();
+        for (Integer x: testData)
+            avl.add(x, null);
+
+        endTime = System.nanoTime();
+
+        time = (endTime - startTime) / 1000000000.0;
+        System.out.println("AVL: " + time + " s");
+
+
+        // Test RBTree
+        startTime = System.nanoTime();
+
+        RBTree<Integer, Integer> rbt = new RBTree<>();
+        for (Integer x: testData)
+            rbt.add(x, null);
+
+        endTime = System.nanoTime();
+
+        time = (endTime - startTime) / 1000000000.0;
+        System.out.println("RBTree: " + time + " s");
+    }
+}
